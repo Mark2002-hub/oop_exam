ae0f0a96c14a247430d4597cf455a8a13d9e8546
MarkKaraush <mark.karaush@gmail.com>
2021-06-15 09:34:21 +0300

    added function is_palindrom()

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <cmath>
+
+bool is_palindrom(const std::string& s)
+{
+    auto length = s.size();
+    for (std::size_t i = 0; i < std::floor(length / 2); ++i)
+    {
+        if (s[i] != s[length - 1 -i])
+            return false;
+    }
+    return true;
+}
 
 int main()
 {
-	std::cout << "Hello world" << std::endl;
-	return 0;
+    std::cout << "Hello world" << std::endl;
+    return 0;
 }
